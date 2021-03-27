Fixed scaling of selected endmembers to match the original tensor

ntf.py
@@ -16,25 +16,37 @@ datapath = '../../MATLAB/ComponentAnalysisExperiments/data/'
 # filename = 'h01-samson'; Lr = 31
 # filename = 'h02-jasper'; Lr = 12
 # filename = 'h03-urban'; Lr = 220
-
+## WHISPERS min(I,J)^2 / R*K
+# filename = 'h01-samson'; Lr = 19
+# filename = 'h02-jasper'; Lr = 12
+# filename = 'h03-urban'; Lr = 145
+## WHISPERS2 min(I,J)^2 / R*min(I,J,K)
+# filename = 'h01-samson'; Lr = 31
+# filename = 'h02-jasper'; Lr = 25
+# filename = 'h03-urban'; Lr = 145
 ## WHISPERS Paper L=(min(I,J)^2)/K
 # 95*95/(156*3) = 19
 # 100^2/(198*4) = 12
 # 307^2/(162*4) = 145
-trials = range(1)
-filenames = ['usgs/synleg-e4s2-01',
+trials = range(5)
+# filenames=[filename]
+# lowranks=[Lr]
+filenames = [
+    'usgs/synleg-e4s2-01',
     'usgs/synleg-e4s2-02',
     'usgs/synleg-e4s2-03',
     'usgs/synleg-e4s2-04']
-# filenames = ['usgs/synleg-e4s2-04']
-# filenames = ['usgs/synt-e4x64-03']
-# filenames = ['h03-urban']
-lowranks = [16]
+filenames = ['h03-urban6']
+lowranks = [200]
+# filenames = ['h02-jasper']
+# lowranks = [19]
+# nem=4
 parms = LrModelParameters()
 parms.lrate = 0.001
 parms.MaxDelta = 1e-8
-parms.RegWeight = 0.
+parms.RegWeight = 0.01
 parms.AscWeight = 0.
+parms.MovAvgCount = 10
 
 AbundanceThreshold = 0.95
 AbundanceFromTarget = False
@@ -51,16 +63,32 @@ for fn in filenames:
         # Sgt = np.random.uniform(size=(Y.shape[2],R))
 
         ### L-inf Normalized on mode-2
-        Y = Y/np.max(Y)
+        # Normalize Integers to float
+        Ymax = np.max(Y)
+        Y = Y/Ymax
+        # Get Max Intensity of each
         Yninf = np.linalg.norm(Y, ord=np.inf, axis=2, keepdims=True)
+        # Get Power of each pixel
         Yn2 = np.linalg.norm(Y, ord=2, axis=2, keepdims=True)
-        Y=Y/Yninf
+        # Normalize Intensity
+        Ynorm = Yninf
+        Y = Y/Ynorm
         
         [I,J,K] = Y.shape
         [K,R] = Sgt.shape
+        # if R > nem :
+        #     Sgt = Sgt[:,0:nem]
+        #     [K,R] = Sgt.shape
+        # elif nem > R:
+        #     Sgt2 = np.random.uniform(size=[K,nem])
+        #     Sgt2[:,0:R]=Sgt
+        #     Sgt=Sgt2
+        #     [K,R] = Sgt.shape
         print(fn)
         print(f'[I,J,K]=>[{I},{J},{K}]   [Lr,R]=>[{Lr},{R}]')
         parms.prnt()
+        # plt.imshow(hsi2rgb(Y))
+        # plt.show()
 
         for i in trials:
             # Instanciate Model
@@ -71,16 +99,24 @@ for fn in filenames:
 
             Sprime = get_endmembers(model, 
                 AbundanceThreshold,
-                norms=Yninf,
+                norms=Ynorm,
                 fromtarget=AbundanceFromTarget)
             (Sprime,p) = reorder(Sprime,Sgt)
             print(f'Reorder: {p}')
             # plot_decomposition(model,Sgt,Sprime,p)
             # plt.show()
 
-            # Compute Fully Constrained Least Squares 
-            A = fcls_np(Y,Sprime)
+            # Compute Fully Constrained Least Squares
+
+            A = fcls_np(Y,Sprime,norms=Ynorm)
             Agt = read_agt(matdict)
+            # [nax,nay]=Agt.shape
+            # if nax>R :
+            #     Agt = Agt[0:R,:]
+            # elif nax<R :
+            #     Agt2 = np.zeros(R,nay)
+            #     Agt2[0:nax,:] = Agt
+            #     Agt = Agt2
             nx = math.floor(matdict['nx'])
             mtx = compute_metrics(i,Sgt, Sprime, A, Agt)
             # plot_abundance(Agt,A,nx)

ntfmodel.py
@@ -50,7 +50,7 @@ class LrModel:
      
         self.opt = tf.keras.optimizers.Adam(learning_rate=self.parms.lrate)
         # self.opt = tf.keras.optimizers.SGD(learning_rate=.01)
-        # self.opt = tf.optimizers.Adagrad(learning_rate=1)
+        # self.opt = tf.optimizers.Adagrad(learning_rate=0.001)
         # self.opt = tf.optimizers.Nadam(learning_rate=0.002, beta_1=0.9, beta_2=0.999)
     
     def Eop(self):
@@ -64,12 +64,6 @@ class LrModel:
         return tf.matmul(self.A,self.B,transpose_b=True)
         
     def __call__(self):
-        self.apply_anc('relu')
-        op1 = self.Eop()
-        op = tf.tensordot(op1,self.C,[0,0])
-        return op
-    
-    def decoder(self):
         self.apply_anc('relu')
         op1 = self.Eop()
         op = tf.tensordot(op1,self.C,[0,0])

utils.py
@@ -4,9 +4,11 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 def hsi2rgb(Y):
-    Yb = np.mean(Y[:,:,0:60],axis=2)
-    Yg = np.mean(Y[:,:,60:120],axis=2)
-    Yr = np.mean(Y[:,:,120:-1],axis=2)
+    [nx,ny,nz] = Y.shape
+    cut = math.floor(nz/3)
+    Yb = np.mean(Y[:,:,0:cut],axis=2)
+    Yg = np.mean(Y[:,:,cut:2*cut],axis=2)
+    Yr = np.mean(Y[:,:,2*cut:-1],axis=2)
     Yrgb = np.stack([Yr,Yg,Yb],axis=2)
     return Yrgb
 
@@ -19,7 +21,7 @@ def plot_decomposition(model, Sgt, Sprime, p):
     Yprime = model().numpy()
     # Normalize
     E = E/np.max(E,axis=(1,2),keepdims=True)
-    C = C/np.max(C,axis=1,keepdims=True)
+    # C = C/np.max(C,axis=1,keepdims=True)
     
     # Plot target tensor and reconstruction
     M=5
@@ -132,9 +134,14 @@ def get_endmembers(model,threshold, norms=1., fromtarget=False, asc=False):
         Em = E1/np.max(E1,axis=(1,2),keepdims=True)
     else:
         Em = E/np.max(E,axis=(1,2),keepdims=True)
-        
-    Yprime = model().numpy()*norms
-    Ytarget = model.Y.numpy()*norms
+    
+    Yprime = model().numpy()
+    Ytarget = model.Y.numpy()
+    # Put intensities back on the reconstruction before
+    # Averaging out candidate endmembers
+    Yprime = Yprime*norms
+    Ytarget = Ytarget*norms
+    
     [_,_,K] = Yprime.shape
     
     Sprime = np.zeros(shape=(K,R),dtype=np.float)
@@ -152,16 +159,15 @@ def get_endmembers(model,threshold, norms=1., fromtarget=False, asc=False):
         Sprime[:,r] = mean_vector.numpy()
     return Sprime
 
-def fcls_np(Yin, Sin):
+def fcls_np(Yin, Sin, norms=1.):
     '''
     A[R,N] = fcls_np(Y[I,J,K], S[K,R])
     Numpy implementation
     fcls_np() Elapsed time: 9.566718578338623 seconds
     rmse: [0.02025357 0.0487208  0.03135294 0.05578483]
     '''
-    # Sin = Sin / np.max(Sin,axis=0,keepdims=True)
-    # Yin = Yin / np.max(Yin,axis=2,keepdims=True)
     t0 = time.time()
+
     Sin = Sin / np.linalg.norm(Sin,axis=0,keepdims=True)
     Yin = Yin / np.linalg.norm(Yin,axis=2,keepdims=True)
 